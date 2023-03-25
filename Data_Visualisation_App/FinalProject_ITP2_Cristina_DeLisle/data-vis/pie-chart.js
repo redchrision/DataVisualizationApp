34c2c803b8e2729dacdb39a03f5be9deebf3af96
Cristina DeLisle <95185329+redchrision@users.noreply.github.com>
2023-03-25 20:19:03 +0100

    Final app

---
+++ Data_Visualisation_App/FinalProject_ITP2_Cristina_DeLisle/data-vis/pie-chart.js
@@ -0,0 +1,99 @@
+function PieChart(x, y, diameter) {
+
+  this.x = x;
+  this.y = y;
+  this.diameter = diameter;
+  this.labelSpace = 30;
+
+  this.get_radians = function(data) {
+    var total = sum(data);
+    var radians = [];
+
+    for (let i = 0; i < data.length; i++) {
+      radians.push((data[i] / total) * TWO_PI);
+    }
+
+    return radians;
+  };
+
+  this.draw = function(data, labels, colours, title) {
+
+    // Test that data is not empty and that each input array is the
+    // same length.
+    if (data.length == 0) {
+      alert('Data has length zero!');
+    } else if (![labels, colours].every((array) => {
+      return array.length == data.length;
+    })) {
+      alert(`Data (length: ${data.length})
+Labels (length: ${labels.length})
+Colours (length: ${colours.length})
+Arrays must be the same length!`);
+    }
+
+    // https://p5js.org/examples/form-pie-chart.html
+
+    var angles = this.get_radians(data);
+    var lastAngle = 0;
+    var colour;
+
+    for (var i = 0; i < data.length; i++) {
+      if (colours) {
+        colour = colours[i];
+      } else {
+        colour = map(i, 0, data.length, 0, 255);
+      }
+
+      fill(colour);
+      stroke(0);
+      strokeWeight(1);
+
+      arc(this.x, this.y,
+          this.diameter, this.diameter,
+          lastAngle, lastAngle + angles[i] + 0.001); // Hack for 0!
+
+
+      //This will display percentages in the centre of each element of the pie charts
+      //Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/cos
+      //Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round 
+      textAlign(CENTER,CENTER);
+      if (angles[i] > 0.10) {
+          const percentageAngle = lastAngle + angles[i] / 2;
+          const percentageX = this.x + this.diameter * Math.cos(percentageAngle) / 3;
+          const percentageY = this.y + this.diameter * Math.sin(percentageAngle) / 3;
+          fill(0,0,0);
+          textSize(15);
+          text(Math.round(100*angles[i]/TWO_PI)+'%', percentageX, percentageY);
+      }
+
+      if (labels) {
+        this.makeLegendItem(labels[i], i, colour);
+      }
+
+      lastAngle += angles[i];
+    }
+
+    if (title) {
+      noStroke();
+      textAlign('center', 'center');
+      textSize(24);
+      text(title, this.x, this.y - this.diameter * 0.6);
+    }
+  };
+
+  this.makeLegendItem = function(label, i, colour) {
+    var x = this.x + 50 + this.diameter / 2;
+    var y = this.y + (this.labelSpace * i) - this.diameter / 3;
+    var boxWidth = this.labelSpace / 2;
+    var boxHeight = this.labelSpace / 2;
+
+    fill(colour);
+    rect(x, y, boxWidth, boxHeight);
+
+    fill('black');
+    noStroke();
+    textAlign('left', 'center');
+    textSize(12);
+    text(label, x + boxWidth + 10, y + boxWidth / 2);
+  };
+}
